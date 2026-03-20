bfd709cc282c380da40e0e8aebf716db026eb123
DaniilIzman <dan200631@gmail.com>
2026-03-20 19:58:55 +0100

    Void method PrintInConsole()
    Created a simple method to display instructions in the scene’s console when the game starts

--- Mover.cs
+++ Mover.cs
@@ -4,9 +4,16 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void PrintInConsole()
+    {
+        Debug.Log("Welcome!");
+        Debug.Log("Use the WASD keys or the arrow keys to control your character’s movement");
+        Debug.Log("Don't bump into objects.");
+    }
     void Start()
     {
-
+        PrintInConsole();
     }
 
     // Update is called once per frame
