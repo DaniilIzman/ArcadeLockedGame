6fdc5adb4f420801273151c5c7f350751c06aaea
DaniilIzman <dan200631@gmail.com>
2026-03-20 15:42:56 +0100

    Variables
    Moved the x, y, and z variables to the update function to support further implementation of keyboard controls for our player

--- Mover.cs
+++ Mover.cs
@@ -2,9 +2,6 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +11,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        [SerializeField] float xValue = 0f;
+        [SerializeField] float yValue = 0.01f;
+        [SerializeField] float zValue = 0f;
         transform.Translate(xValue, yValue, zValue);
     }
 }
