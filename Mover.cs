8fc6b64be82be85280b8960cd813f7c2651c6801
DaniilIzman <dan200631@gmail.com>
2026-03-20 20:11:16 +0100

    void MovePlayer() method + small code restructure
    Created a separate function for player movement and called it in Update(). Also moved the PrintInConsole method lower in the code for improved readability

--- Mover.cs
+++ Mover.cs
@@ -4,13 +4,6 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-    void PrintInConsole()
-    {
-        Debug.Log("Welcome!");
-        Debug.Log("Use the WASD keys or the arrow keys to control your character’s movement");
-        Debug.Log("Don't bump into objects.");
-    }
     void Start()
     {
         PrintInConsole();
@@ -23,5 +16,18 @@ public class Mover : MonoBehaviour
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    // Information in console
+    void PrintInConsole()
+    {
+        Debug.Log("Welcome!");
+        Debug.Log("Use the WASD keys or the arrow keys to control your character’s movement");
+        Debug.Log("Don't bump into objects.");
     }
 }
