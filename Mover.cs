bdc23f64dc7ceb1ea6e800dd2ee96d62c831c0a7
DaniilIzman <dan200631@gmail.com>
2026-03-20 18:00:41 +0100

    More consistent movement speed and adjustable speed
    Previously, the code relied on the power of the user’s PC for movement speed (before using Time.deltaTime), making it inconsistent across different systems. This has now been fixed by incorporating Time.deltaTime. Additionally, a moveSpeed variable has been added, allowing the user to adjust the player object's speed through the Inspector in the scene settings

--- Mover.cs
+++ Mover.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
     }
 }
