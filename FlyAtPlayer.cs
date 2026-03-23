5bdba231bd11b2960a5d6dd05f2d910b2f95f5f0
DaniilIzman <dan200631@gmail.com>
2026-03-23 17:54:44 +0100

    DestroyOnReach method
    Added a method to the FlyAtPlayer script so that when the game object reaches its final destination, it is destroyed to prevent overloading the system.Also created seperate ShootAtplayer method

--- FlyAtPlayer.cs
+++ FlyAtPlayer.cs
@@ -13,6 +13,18 @@ public class FlyAtPlayer : MonoBehaviour
 
     void Update()
     {
+        ShootAtPlayer();
+        DestroyOnReach();
+    }
+    void ShootAtPlayer()
+    {
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime * ProjectileSpeed);
+    }
+    void DestroyOnReach()
+    {
+        if(transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
     }
 }
