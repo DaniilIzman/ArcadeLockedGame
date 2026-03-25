94ba22cf242611ef98b50f13e4e423bb3e906d77
DaniilIzman <dan200631@gmail.com>
2026-03-25 20:12:57 +0100

    ObjectHitAction - Customizable color
    Added a variable to allow the user to choose the color of an object when it gets hit

--- ObjectHitAction.cs
+++ ObjectHitAction.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class ObjectHitAction : MonoBehaviour
 {
+    [SerializeField] Color ColorHit;
     void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "Player")
         {
-            GetComponent<MeshRenderer>().material.color = Color.darkOliveGreen;
+            GetComponent<MeshRenderer>().material.color = ColorHit;
             gameObject.tag = "Hit";
         }
     }
