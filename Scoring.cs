a60a2a3c6af77011fab6c15132971fbfe9c944c3
DaniilIzman <dan200631@gmail.com>
2026-03-25 17:18:11 +0100

    Scoring - BumpMaxActivate
    Added an additional setting for the scoring system - BumpMaxActivate. This allows enabling or disabling the game over condition in the Inspector and was added for debugging

--- Scoring.cs
+++ Scoring.cs
@@ -5,6 +5,7 @@ public class Scoring : MonoBehaviour
 {
     
     [SerializeField] int BumpMax;
+    [SerializeField] bool BumpMaxActivate;
     int bumpCounter = 0;
 
     void OnCollisionEnter(Collision other)
@@ -13,7 +14,7 @@ public class Scoring : MonoBehaviour
         {
             bumpCounter++;
             Debug.Log("You've bumped into objects " + bumpCounter + " times");
-            if(bumpCounter == BumpMax)
+            if(bumpCounter == BumpMax && BumpMaxActivate == true)
             {
                 Debug.Log("Game Over!");
                 Time.timeScale = 0f;
