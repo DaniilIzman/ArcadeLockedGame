4a6b3b6bce60eb6c70619ac0c9bae78a7560b3ff
DaniilIzman <dan200631@gmail.com>
2026-03-27 19:52:32 +0100

    ObjectHitAction - Material reset
    Implemented a simple color reset when the object is initially hit, along with a timer for it

--- ObjectHitAction.cs
+++ ObjectHitAction.cs
@@ -3,12 +3,40 @@ using UnityEngine;
 public class ObjectHitAction : MonoBehaviour
 {
     [SerializeField] Color ColorHit;
+    [SerializeField] bool ResetAfterTimer;
+    [SerializeField] float ResetTimer = 0f;
+    bool ObjectTouched;
+    Color defaultColor;
+
+    void Start()
+    {
+        defaultColor = GetComponent<MeshRenderer>().material.color;
+    }
+    void Update()
+    {
+        if(ResetAfterTimer == true && ObjectTouched == true)
+        {
+            ResetToDefault();
+        }
+    }
     void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "Player")
         {
+            ObjectTouched = true;
             GetComponent<MeshRenderer>().material.color = ColorHit;
             gameObject.tag = "Hit";
+        }
+    }
+    void ResetToDefault()
+    {
+        if(ResetAfterTimer == true && ResetTimer >= 0f)
+        {
+            ResetTimer -= Time.deltaTime;
+            if(ResetTimer <= 0f)
+            {
+                GetComponent<MeshRenderer>().material.color = defaultColor;
+            }
         }
     }
 }
