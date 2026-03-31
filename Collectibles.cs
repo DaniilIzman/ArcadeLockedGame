d6ceea4b5671dbc74e601f8bbc6e1e724148b185
DaniilIzman <dan200631@gmail.com>
2026-03-31 20:59:06 +0200

    Collectibles – small adjustments and logic fixes
    Commented out variables FreezeTimer, SpeedMultiplier, and InvulnerabilityTimer to prevent errors when compiling the game. Also fixed the logic for the HP collectible; previously, it was adding additional bumps allowed even if the player object had the bump counter disabled

--- Collectibles.cs
+++ Collectibles.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 
-public class Collectables : MonoBehaviour
+public class Collectibles : MonoBehaviour
 {
-    [SerializeField] float FreezeTimer = 0f;
-    [SerializeField] float SpeedMultiplier = 0f;
+    //[SerializeField] float FreezeTimer = 0f;
+    //[SerializeField] float SpeedMultiplier = 0f;
     [SerializeField] int AdditionalBumps = 0;
-    [SerializeField] float InvulnerabilityTimer = 0f;
+    //[SerializeField] float InvulnerabilityTimer = 0f;
     void OnTriggerEnter(Collider other)
     {
-        if(gameObject.tag == "HP" && other.gameObject.tag == "Player")
+        Scoring scoring = FindAnyObjectByType<Scoring>();
+        if(gameObject.tag == "HP" && other.gameObject.tag == "Player" && scoring.BumpMaxActivate == true)
         {
-            Scoring scoring = FindAnyObjectByType<Scoring>();
             scoring.bumpCounter -= AdditionalBumps;
             scoring.bumpCounter = Mathf.Max(scoring.bumpCounter, 0);
+            Destroy(gameObject);
+        }
+        else if(other.gameObject.tag == "Player")
+        {
             Destroy(gameObject);
         }
     }
