15721b9530d89f02e71d722b99a38667e4d3f363
DaniilIzman <dan200631@gmail.com>
2026-04-01 18:59:03 +0200

    Collectibles - Freeze object implementation
    Freezer pickup object was implemented with appropriate checks and new boolean variables. Also reconstructed the code slightly using FindAnyObjectByType for Scoring and Mover references to simplify component access. Added logic in Update to handle the freeze timer countdown

--- ObstacleMaze/Collectibles.cs
+++ ObstacleMaze/Collectibles.cs
@@ -2,13 +2,33 @@ using UnityEngine;
 
 public class Collectibles : MonoBehaviour
 {
-    //[SerializeField] float FreezeTimer = 0f;
+    [SerializeField] float FreezeTimer = 0f;
     //[SerializeField] float SpeedMultiplier = 0f;
     [SerializeField] int AdditionalBumps = 0;
     //[SerializeField] float InvulnerabilityTimer = 0f;
+    bool frozen = false;
+    Scoring scoring;
+    Mover mover;
+    void Awake()
+    {
+        scoring = FindAnyObjectByType<Scoring>();
+        mover = FindAnyObjectByType<Mover>();
+    }
+    void Update()
+    {
+        if(frozen == true && FreezeTimer > 0f)
+        {
+            FreezeTimer -= Time.deltaTime;
+            if(FreezeTimer <= 0f)
+            {
+                frozen = false;
+                mover.canMove = true;
+                Destroy(gameObject);
+            }
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
-        Scoring scoring = FindAnyObjectByType<Scoring>();
         if(gameObject.tag == "HP" && other.gameObject.tag == "Player" && scoring.BumpMaxActivate == true)
         {
             if(scoring.bumpCounter > 0)
@@ -17,6 +37,11 @@ public class Collectibles : MonoBehaviour
                 scoring.bumpCounter = Mathf.Max(scoring.bumpCounter, 0);
                 Destroy(gameObject);
             }
-        } 
+        }
+        else if(gameObject.tag == "Freezer" && other.gameObject.tag == "Player")
+        {
+            mover.canMove = false;
+            frozen = true;
+        }
     }
 }
