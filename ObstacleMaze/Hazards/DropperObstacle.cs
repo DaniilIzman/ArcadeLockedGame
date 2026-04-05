abfd8406c2b968141917772aa91d0a89895d5a8d
DaniilIzman <dan200631@gmail.com>
2026-04-05 18:44:21 +0200

    Dropper - trigger support
    Implemented trigger object support for the Dropper script.Now, if the trigger has a dropper object in its list, the falling object will activate upon touching the trigger area.In this iteration of the code, the timer for the falling object does not work with the trigger (to be fixed).Also removed the boolean variable FallOnTrigger since it is not needed - script already checks if the object is in the trigger object list and deactivates it (similarly to the FlyAtPlayer projectile script)

--- ObstacleMaze/Hazards/DropperObstacle.cs
+++ ObstacleMaze/Hazards/DropperObstacle.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 public class DropperObstacle : MonoBehaviour
 {
     [SerializeField] bool FallAfterTimer;
-    [SerializeField] float TimeBeforeFall = 3f;
+    [SerializeField] float TimeBeforeFall = 0f;
     [SerializeField] bool StaysOnTheGround;
     [SerializeField] float GroundTimer = 0f;
-    [SerializeField] bool FallOnTrigger;
+    TriggerHazard Trigger;
     MeshRenderer myMeshRenderer;
     bool OnGround = false;
     
     Rigidbody myRigidBody;
+
+    void Awake() 
+    {
+        Trigger = FindAnyObjectByType<TriggerHazard>();
+        if(Trigger.GameObjectList.Contains(gameObject))
+        {
+            gameObject.SetActive(false); 
+        }
+    }
     void Start()
     {
         myMeshRenderer = GetComponent<MeshRenderer>();
@@ -22,20 +31,21 @@ public class DropperObstacle : MonoBehaviour
 
     void Update()
     {
-        if(Time.time > TimeBeforeFall)
+        if(FallAfterTimer == true && Time.time >= TimeBeforeFall)
         {
             myMeshRenderer.enabled = true;
             myRigidBody.useGravity = true;
-        }
-        if(OnGround == true && GroundTimer >= 0f)
-        {
-            myRigidBody.useGravity = false;
-            GroundTimer -= Time.deltaTime;
-            if(GroundTimer <= 0f)
+            if(OnGround == true && GroundTimer >= 0f)
             {
-                Destroy(gameObject);
+                myRigidBody.useGravity = false;
+                GroundTimer -= Time.deltaTime;
+                if(GroundTimer <= 0f)
+                {
+                    Destroy(gameObject);
+                }
             }
         }
+
     }
     void OnCollisionEnter(Collision other)
     {
