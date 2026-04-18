bba2425648ed7f645c13caabdbc6ca7962e75987
DaniilIzman <dan200631@gmail.com>
2026-04-18 16:43:08 +0200

    Spikes - simple behaviour test
    Implemented a simple spike action where, when the timer reaches the activation time, the object moves to a different position (currently the movement is incorrect, as the object moves on the wrong axis - to be fixed). In this iteration of the code, the object does not return to its initial position -to be fixed

--- ObstacleMaze/Hazards/Spikes.cs
+++ ObstacleMaze/Hazards/Spikes.cs
@@ -5,15 +5,36 @@ public class Spikes : MonoBehaviour
     [SerializeField] float UpValue = 0f;
     [SerializeField] float ActivationTimer = 0f;
     [SerializeField] float SpikedTimer = 0f;
+    float timer = 0f;
     float SaveInitialTimer = 0f;
+    bool IsUp = false;
+    Transform SpikePosition;
+    Vector3 InitialPosition;
+    Vector3 upPosition;
     void Start()
     {
-        SaveInitialTimer = ActivationTimer;
+        InitialPosition = SpikePosition.transform.position;
+        upPosition = InitialPosition + new Vector3(0f, UpValue, 0f);
     }
 
     void Update()
     {
-        if(Time.time >= ActivationTimer + SaveInitialTimer)
-        transform.Translate(0f, UpValue, 0f);
+        SpikesBehaviour();
+    }
+    void SpikesBehaviour()
+    {
+        timer += Time.deltaTime;
+        if(!IsUp && timer >= ActivationTimer)
+        {
+            transform.position = upPosition;
+            IsUp = true;
+            timer = 0f;
+        }
+        else if(IsUp && timer >= SpikedTimer)
+        {
+            transform.position = InitialPosition;
+            IsUp = false;
+            timer = 0f;
+        }
     }
 }
