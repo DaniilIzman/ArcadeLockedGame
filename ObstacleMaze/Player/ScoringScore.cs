7d4f662675ee70cf9f4a1767928bde8a4e225a4a
DaniilIzman <dan200631@gmail.com>
2026-04-23 20:43:25 +0200

    ScoringScore - Simple scoring system
    Implemented a simple scoring system mechanic for the player. The script takes the initial value of the player when starting the game and compares it with the difference between the current player position and the last position. If the difference value is 0, console tells the player that they are standing still and need to move forward. If the difference value is greater than 0, score increases (by adding the difference in distance to the initial ScoreCounter value), and the console indicates that the player is moving forward. If the difference value is less than 0, it results in a penalty for the player. This iteration does not work properly and was created to experiment with a simple scoring mechanic

--- ObstacleMaze/Player/ScoringScore.cs
+++ ObstacleMaze/Player/ScoringScore.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class ScoringScore : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float ScoreCounter = 0f;
+    Vector3 lastPosition;
+    float MoveCheck = 0f;
+    [SerializeField] float ScorePenalty = 0f;
     void Start()
     {
-        
+        lastPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        MoveCheck = transform.position.z - lastPosition.z;
         
+        if(MoveCheck > 0f)
+        {
+            ScoreCounter += MoveCheck;
+            Debug.Log("Moving forward. Score: " + ScoreCounter);
+        }
+
+        else if(MoveCheck < 0)
+        {
+            ScoreCounter -= ScorePenalty;
+            Debug.Log("Moving back. Score penalty: " + ScoreCounter);
+        }
+
+        else
+        {
+            Debug.Log("Standing still. Move!");
+        }
+        lastPosition = transform.position;
+
     }
 }
