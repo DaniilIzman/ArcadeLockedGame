87504857d6e862bd3a0413b84f9a4d57f413cc12
DaniilIzman <dan200631@gmail.com>
2026-04-24 18:45:22 +0200

    ScoringScore - Code removal
    Removed the old calculation for the player's position for the further rework of the player scoring system

--- ObstacleMaze/Player/ScoringScore.cs
+++ ObstacleMaze/Player/ScoringScore.cs
@@ -7,32 +7,9 @@ public class ScoringScore : MonoBehaviour
     public float MoveCheck = 0f;
     public float ScoreMultiplier = 1f;
     [SerializeField] float ScorePenalty = 0f;
-    void Start()
-    {
-        lastPosition = transform.position;
-    }
 
     void Update()
     {
-        MoveCheck = transform.position.z - lastPosition.z;
-        
-        if(MoveCheck > 0f)
-        {
-            ScoreCounter += MoveCheck * ScoreMultiplier;
-            Debug.Log("Moving forward. Score: " + ScoreCounter);
-        }
-
-        else if(MoveCheck < 0)
-        {
-            ScoreCounter += MoveCheck * ScorePenalty * ScoreMultiplier;
-            Debug.Log("Moving back. Score penalty: " + ScoreCounter);
-        }
-
-        else
-        {
-            Debug.Log("Standing still. Move!");
-        }
-        lastPosition = transform.position;
-
+        ScoreCounter += MoveCheck * ScoreMultiplier;
     }
 }
