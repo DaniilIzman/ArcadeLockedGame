9dabf575f268febef7a8b8ba75f52e2989866580
DaniilIzman <dan200631@gmail.com>
2026-04-24 20:03:10 +0200

    ScoringScore - Unused code lines
    Removed unused variables

--- ObstacleMaze/Player/ScoringScore.cs
+++ ObstacleMaze/Player/ScoringScore.cs
@@ -3,11 +3,8 @@ using UnityEngine;
 public class ScoringScore : MonoBehaviour
 {
     public float ScoreCounter = 0f;
-    Vector3 lastPosition;
     public float MoveCheck = 0f;
     public float ScoreMultiplier = 1f;
-    [SerializeField] float ScorePenalty = 0f;
-
     void Update()
     {
         ScoreCounter += MoveCheck * ScoreMultiplier;
