cc2ac859738d9638fbb5d6804eb7d01be42c362d
DaniilIzman <dan200631@gmail.com>
2026-04-25 14:18:51 +0200

    Spin - Unused lines
    Removed lines of code from the spin script since they are no longer needed

--- ObstacleDodge/Scripts/Spin.cs
+++ ObstacleDodge/Scripts/Spin.cs
@@ -6,9 +6,6 @@ public class Spin : MonoBehaviour
     [SerializeField] float RotateX = 0f;
     [SerializeField] float RotateY = 0f;
     [SerializeField] float RotateZ = 0f;
-    [SerializeField] bool SlowAfterHit;
-    [SerializeField] float SpeedMultiplier = 0f;
-    float CurrentMultiplier = 1f;
     ObjectHitAction HitAction;
     void Awake()
     {
@@ -16,18 +13,6 @@ public class Spin : MonoBehaviour
     }
     void Update()
     {
-        if(SlowAfterHit)
-        {
-            if(HitAction.ObjectTouched)
-            {
-                CurrentMultiplier = SpeedMultiplier;
-            }
-            else
-            {
-                CurrentMultiplier = 1f;   
-            }
-
-        }
-        transform.Rotate(RotateX * CurrentMultiplier, RotateY * CurrentMultiplier, RotateZ * CurrentMultiplier);
+        transform.Rotate(RotateX, RotateY, RotateZ);
     }
 }
