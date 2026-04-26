aa30fda5b92fc9c3c968e8ebfbcbbe58ca89a061
DaniilIzman <dan200631@gmail.com>
2026-04-26 17:40:52 +0200

    NewSectionCreator - Minor fix
    Added gameObject to the tag comparison to stay consistent with the Tile script

--- ObstacleDodge/Scripts/Player/NewSectionCreator.cs
+++ ObstacleDodge/Scripts/Player/NewSectionCreator.cs
@@ -5,7 +5,7 @@ public class NewSectionCreator : MonoBehaviour
     public GameObject Tile;
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("TriggerExtender"))
+        if(other.gameObject.CompareTag("TriggerExtender"))
         {
             Instantiate(Tile, new Vector3(7, 2, 35), Quaternion.identity);
         }
