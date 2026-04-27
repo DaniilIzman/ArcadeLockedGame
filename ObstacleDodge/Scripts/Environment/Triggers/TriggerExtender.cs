5b283513a3dc90161e016c9bf189e8f6b07f4522
DaniilIzman <dan200631@gmail.com>
2026-04-27 16:44:34 +0200

    TriggerExtender script
    Created a separate script for the tile extender with an adjustable spawn offset and a selectable tile game object

---
+++ ObstacleDodge/Scripts/Environment/Triggers/TriggerExtender.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class TriggerExtender : MonoBehaviour
+{
+    public GameObject TilePrefab;      // What tile to spawn next
+    public Vector3 SpawnOffset;        // Relative to this trigger's position
+}
