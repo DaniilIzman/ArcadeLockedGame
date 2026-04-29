6269d48cf10a5dba0dd60145e5084e25e1ce3f19
DaniilIzman <dan200631@gmail.com>
2026-04-29 16:27:55 +0200

    TriggerExtender - Comments removal
    Removed comments from TriggerExtender script

--- ObstacleDodge/Scripts/Environment/Triggers/TriggerExtender.cs
+++ ObstacleDodge/Scripts/Environment/Triggers/TriggerExtender.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 public class TriggerExtender : MonoBehaviour
 {
-    public GameObject TilePrefab;      // What tile to spawn next
-    public Vector3 SpawnOffset;        // Relative to this trigger's position
+    public GameObject TilePrefab;
+    public Vector3 SpawnOffset;
 }
