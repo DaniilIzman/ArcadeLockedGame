fe20dc09de25edbaf62ee83b839dd7443cee2f64
DaniilIzman <dan200631@gmail.com>
2026-04-29 17:56:06 +0200

    NewTileCreator - Simple object randomizer
    Implemented a simple randomized object spawn on tiles for collectibles and obstacles. This iteration of the code does not work properly, as objects spawn outside the tile (to be fixed)

--- ObstacleDodge/Scripts/Player/NewTileCreator.cs
+++ ObstacleDodge/Scripts/Player/NewTileCreator.cs
@@ -2,13 +2,37 @@ using UnityEngine;
 
 public class NewTileCreator : MonoBehaviour
 {
+    public float SpawnRangeX = 3f;
+    public float SpawnRangeZ = 3f;
+
     void OnTriggerEnter(Collider other)
     {
         TriggerExtender extender = other.GetComponent<TriggerExtender>();
         Vector3 spawnPosition = extender.transform.position + extender.SpawnOffset;
-        if(other.gameObject.CompareTag("TriggerExtender"))
+
+        if (other.gameObject.CompareTag("TriggerExtender"))
         {
-            Instantiate(extender.TilePrefab, spawnPosition, Quaternion.identity);      
+            Instantiate(extender.TilePrefab, spawnPosition, Quaternion.identity);
+
+            if (extender.Obstacles != null && extender.Obstacles.Length > 0)
+            {
+                foreach (GameObject obstacle in extender.Obstacles)
+                {
+                    Vector3 obstacleSpawnPos = new Vector3(
+                    spawnPosition.x + Random.Range(-SpawnRangeX, SpawnRangeX),spawnPosition.y, spawnPosition.z + Random.Range(-SpawnRangeZ, SpawnRangeZ));
+                    Instantiate(obstacle, obstacleSpawnPos, Quaternion.identity);
+                }
+            }
+
+            if (extender.Collectibles != null && extender.Collectibles.Length > 0)
+            {
+                foreach (GameObject collectible in extender.Collectibles)
+                {
+                    Vector3 collectibleSpawnPos = new Vector3(
+                        spawnPosition.x + Random.Range(-SpawnRangeX, SpawnRangeX),spawnPosition.y,spawnPosition.z + Random.Range(-SpawnRangeZ, SpawnRangeZ));
+                    Instantiate(collectible, collectibleSpawnPos, Quaternion.identity);
+                }
+            }
         }
     }
 }
