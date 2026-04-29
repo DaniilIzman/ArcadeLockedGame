ef75eebaa781dbc46095174bfc43b42ecc4e2a84
DaniilIzman <dan200631@gmail.com>
2026-04-29 17:15:46 +0200

    TriggerExtender - Arrays
    Added arrays and adjustable coordinates for collectible and obstacle objects for further implementation of randomized spawning of them

--- ObstacleDodge/Scripts/Environment/Triggers/TriggerExtender.cs
+++ ObstacleDodge/Scripts/Environment/Triggers/TriggerExtender.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 public class TriggerExtender : MonoBehaviour
 {
     public GameObject TilePrefab;
+    public GameObject[] Collectibles;
+    public GameObject[] Obstacles;
     public Vector3 SpawnOffset;
+    public Vector3 SpawnOffsetCollectibles;
+    public Vector3 SpawnOffsetObstacles;
 }
