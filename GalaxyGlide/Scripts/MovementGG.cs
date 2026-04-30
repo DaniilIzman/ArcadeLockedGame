29946c2b90714fd948f0e588697e5bff617a7080
DaniilIzman <dan200631@gmail.com>
2026-04-30 23:05:30 +0200

    MovementGG - Up movement
    Implemented simple and adjustable upward movement using AddRelativeForce, as it applies force relative to the object’s rotation. Also changed the Update method to FixedUpdate, as it is more appropriate for physics-based movement (which is needed for the player’s movement in GG)

--- GalaxyGlide/Scripts/MovementGG.cs
+++ GalaxyGlide/Scripts/MovementGG.cs
@@ -4,17 +4,22 @@ using UnityEngine.InputSystem;
 public class MovementGG : MonoBehaviour
 {
     [SerializeField] InputAction Up;
-
+    [SerializeField] float thrustForce = 0f;
+    Rigidbody myRigidBody;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
     void OnEnable()
     {
         Up.Enable();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if(Up.IsPressed())
         {
-            Debug.Log("Up button is pressed!");
+            myRigidBody.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
         }
     }
 }
