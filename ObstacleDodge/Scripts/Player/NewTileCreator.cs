f4e9b7064871d00361fc7565cb80590701b857d4
DaniilIzman <dan200631@gmail.com>
2026-04-30 16:22:53 +0200

    NewTileCreator - Reverted
    Reverted the code to the previous version because randomized spawning of objects would become too messy. Instead, the code will randomize tile variations in a future iteration

--- ObstacleDodge/Scripts/Player/NewTileCreator.cs
+++ ObstacleDodge/Scripts/Player/NewTileCreator.cs
@@ -1,38 +1,13 @@
 using UnityEngine;
 
-public class NewTileCreator : MonoBehaviour
+public class NewSectionCreator : MonoBehaviour
 {
-    public float SpawnRangeX = 3f;
-    public float SpawnRangeZ = 3f;
-
+    public GameObject Tile;
     void OnTriggerEnter(Collider other)
     {
-        TriggerExtender extender = other.GetComponent<TriggerExtender>();
-        Vector3 spawnPosition = extender.transform.position + extender.SpawnOffset;
-
-        if (other.gameObject.CompareTag("TriggerExtender"))
+        if(other.gameObject.CompareTag("TriggerExtender"))
         {
-            Instantiate(extender.TilePrefab, spawnPosition, Quaternion.identity);
-
-            if (extender.Obstacles != null && extender.Obstacles.Length > 0)
-            {
-                foreach (GameObject obstacle in extender.Obstacles)
-                {
-                    Vector3 obstacleSpawnPos = new Vector3(
-                    spawnPosition.x + Random.Range(-SpawnRangeX, SpawnRangeX),spawnPosition.y, spawnPosition.z + Random.Range(-SpawnRangeZ, SpawnRangeZ));
-                    Instantiate(obstacle, obstacleSpawnPos, Quaternion.identity);
-                }
-            }
-
-            if (extender.Collectibles != null && extender.Collectibles.Length > 0)
-            {
-                foreach (GameObject collectible in extender.Collectibles)
-                {
-                    Vector3 collectibleSpawnPos = new Vector3(
-                        spawnPosition.x + Random.Range(-SpawnRangeX, SpawnRangeX),spawnPosition.y,spawnPosition.z + Random.Range(-SpawnRangeZ, SpawnRangeZ));
-                    Instantiate(collectible, collectibleSpawnPos, Quaternion.identity);
-                }
-            }
+            Instantiate(Tile, new Vector3(7, 2, 42), Quaternion.identity);
         }
     }
 }
