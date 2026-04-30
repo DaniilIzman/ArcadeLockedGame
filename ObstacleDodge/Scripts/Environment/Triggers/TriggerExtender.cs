c6d1670afac38862774dd5b1bf365e663b5dc7eb
DaniilIzman <dan200631@gmail.com>
2026-04-30 16:25:50 +0200

    TriggerExtender - Reverted
    Removed all collectible and obstacle related lines because they are not needed, as the game will randomize tile variants instead of randomly placing objects on newly created tiles

--- ObstacleDodge/Scripts/Environment/Triggers/TriggerExtender.cs
+++ ObstacleDodge/Scripts/Environment/Triggers/TriggerExtender.cs
@@ -3,9 +3,5 @@ using UnityEngine;
 public class TriggerExtender : MonoBehaviour
 {
     public GameObject TilePrefab;
-    public GameObject[] Collectibles;
-    public GameObject[] Obstacles;
     public Vector3 SpawnOffset;
-    public Vector3 SpawnOffsetCollectibles;
-    public Vector3 SpawnOffsetObstacles;
 }
