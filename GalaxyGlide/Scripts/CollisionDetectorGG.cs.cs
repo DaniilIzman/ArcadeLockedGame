45a8e249c207c1740c1b654f0d7c6cfcd93ed8d6
DaniilIzman <dan200631@gmail.com>
2026-05-02 18:27:34 +0200

    CollisionDetectorGG - Level change fix
    Fixed the issue where the script would try to load a scene that does not exist by creating a separate int variable. This variable adds 1 to the currentScene value and compares it to the total number of scenes in the project. If the condition is true, the value of nextScene is reset to 0

--- GalaxyGlide/Scripts/CollisionDetectorGG.cs.cs
+++ GalaxyGlide/Scripts/CollisionDetectorGG.cs.cs
@@ -30,7 +30,12 @@ public class CollisionDetectorGG : MonoBehaviour
         void LoadNextLevelScene()
         {
             int currentScene = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentScene + 1);
+            int nextScene = currentScene + 1;
+            if(nextScene == SceneManager.sceneCountInBuildSettings)
+            {
+                nextScene = 0;
+            }
+            SceneManager.LoadScene(nextScene);
         }
     }
 }
