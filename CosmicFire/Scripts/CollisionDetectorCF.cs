865774ff244957622d8009ef6c75005a380f066f
DaniilIzman <dan200631@gmail.com>
2026-05-07 19:36:58 +0200

    CollisionDetectorCF - Console output
    Added a simple console output when colliding with other objects

--- CosmicFire/Scripts/CollisionDetectorCF.cs
+++ CosmicFire/Scripts/CollisionDetectorCF.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class CollisionDetectorCF : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Hit object: " + other.name);
     }
 }
