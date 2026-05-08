b1350b0a94f9cea004e4094543c27634c3658ca0
DaniilIzman <dan200631@gmail.com>
2026-05-08 17:48:51 +0200

    EnemyCF - Destroy particle
    Added destroy particle effects upon laser particle collision. To implement this, the Instantiate method was used with the following declaration: Instantiate(Object original, Vector3 position, Quaternion rotation). This declaration was used to ensure that the explosion particle spawns at the same position and rotation as the enemy object. Instantiate spawns the prefab on the enemy object.

--- CosmicFire/Scripts/EnemyCF.cs
+++ CosmicFire/Scripts/EnemyCF.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class EnemyCF : MonoBehaviour
 {
+    [SerializeField] GameObject destroyVFX;
     void OnParticleCollision(GameObject other)
     {
+        Instantiate(destroyVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
