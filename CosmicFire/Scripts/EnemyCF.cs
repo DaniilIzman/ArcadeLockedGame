ed652ac71a5e0ef77bb82476cf8635b6c3b8c08d
DaniilIzman <dan200631@gmail.com>
2026-05-08 19:07:18 +0200

    EnemyCF - Enemy hit points
    Implemented a simple HP mechanic for the enemy object. To achieve this, new int variable was added, along with the appropriate check inside the new EnemyHp method

--- CosmicFire/Scripts/EnemyCF.cs
+++ CosmicFire/Scripts/EnemyCF.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class EnemyCF : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX;
+    [SerializeField] int hitPoints = 1;
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyVFX, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        EnemyHP();
+    }
+
+    private void EnemyHP()
+    {
+        hitPoints = hitPoints - 1;
+        if (hitPoints <= 0)
+        {
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
