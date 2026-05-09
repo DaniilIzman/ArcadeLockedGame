64844fa69d78c5f312aab283cb91333dddfd61dd
DaniilIzman <dan200631@gmail.com>
2026-05-09 20:31:20 +0200

    MusicPlayer - Do not restart music clip
    Now the music does not restart from the beginning when the player object gets destroyed - it continues playing

--- CosmicFire/Scripts/MusicPlayer.cs
+++ CosmicFire/Scripts/MusicPlayer.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+       int numberOfMusicPlayers = FindObjectsByType<MusicPlayer>(FindObjectsSortMode.None).Length;
+       if(numberOfMusicPlayers > 1)
+       {
+            Destroy(gameObject);
+       }
+       else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
