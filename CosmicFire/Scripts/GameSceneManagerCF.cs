1ba832b1a0d209273914505a2cedd85f1b5fe7d1
DaniilIzman <dan200631@gmail.com>
2026-05-09 18:47:42 +0200

    GameSceneManagerCF - Proper level reload
    Implemented level reload when the player object gets destroyed

--- CosmicFire/Scripts/GameSceneManagerCF.cs
+++ CosmicFire/Scripts/GameSceneManagerCF.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameSceneManagerCF : MonoBehaviour
 {
 
     public void ReloadLevel()
     {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
         Debug.Log("Reload scene");
     }
 }
