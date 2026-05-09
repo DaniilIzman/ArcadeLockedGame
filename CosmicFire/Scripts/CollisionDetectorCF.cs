f0aaa0e9bef436366ed9fe4cce3c8b1932019650
DaniilIzman <dan200631@gmail.com>
2026-05-09 18:39:04 +0200

    GameSceneManagerCF - Reload level
    Added a simple console output for when the player is destroyed. To implement this, a new method was created inside the GSMCF class, and this method is referenced in the CDCF class

--- CosmicFire/Scripts/CollisionDetectorCF.cs
+++ CosmicFire/Scripts/CollisionDetectorCF.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class CollisionDetectorCF : MonoBehaviour
 {
     [SerializeField] GameObject destroyVFX;
-    
+    GameSceneManagerCF gameSceneManagerCF;
+
+    void Start()
+    {
+        gameSceneManagerCF = FindFirstObjectByType<GameSceneManagerCF>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        gameSceneManagerCF.ReloadLevel();
         Instantiate(destroyVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
