e8c4d8354dd7fd82dda78540b12424a31eeb82ed
DaniilIzman <dan200631@gmail.com>
2026-05-09 18:54:48 +0200

    GameSceneManagerCF - Reload level delay
    Implemented a simple level reload delay using a coroutine and a new adjustable float variable in the Inspector

--- CosmicFire/Scripts/GameSceneManagerCF.cs
+++ CosmicFire/Scripts/GameSceneManagerCF.cs
@@ -1,12 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameSceneManagerCF : MonoBehaviour
 {
 
+    [SerializeField] float reloadLevelDelay = 0f;
     public void ReloadLevel()
     {
+        StartCoroutine(ReloadLevelRoutine());
+    }
+
+    IEnumerator ReloadLevelRoutine()
+    {
+        yield return new WaitForSeconds(reloadLevelDelay);
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
-        Debug.Log("Reload scene");
     }
 }
