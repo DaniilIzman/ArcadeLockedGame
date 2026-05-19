c1232d94c2e31b3c7268260120da9b69f4c71e63
DaniilIzman <dan200631@gmail.com>
2026-05-19 20:44:40 +0200

    HumanMovementAMI - Functionality
    Implemented basic movement for the human player in the arcade room. Space.World was used because otherwise the movement buttons would be inverted, since Translate moves the object based on the direction it is facing by default

--- ArcadeMachinesInteractions/Scripts/HumanMovementAMI.cs
+++ ArcadeMachinesInteractions/Scripts/HumanMovementAMI.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class HumanMovementAMI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
     }
 }
