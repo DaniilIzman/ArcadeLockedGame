c5385b110a9a2ba42885ec709fbb013b035afe6b
DaniilIzman <dan200631@gmail.com>
2026-05-19 17:28:49 +0200

    CameraHandler - Functionality
    Implemented functionality for the camera to follow the player object

--- ArcadeMachinesInteractions/Scripts/CameraHandler.cs
+++ ArcadeMachinesInteractions/Scripts/CameraHandler.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class CameraHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Transform cameraPosition;
     void Update()
     {
-        
+        transform.position = cameraPosition.position;
     }
 }
