55e32d23593b09739f4192d31283af1bdaa0b394
DaniilIzman <dan200631@gmail.com>
2026-05-19 17:54:16 +0200

    HumanCameraAMI - Code rewrite
    Rewrote the code because the previous iteration did not work and required an additional script file. This current iteration works partially, because the camera can only move on the X axis

--- ArcadeMachinesInteractions/Scripts/HumanCameraAMI.cs
+++ ArcadeMachinesInteractions/Scripts/HumanCameraAMI.cs
@@ -2,26 +2,25 @@ using UnityEngine;
 
 public class HumanCameraAMI : MonoBehaviour
 {
-    public float sensitivityX;
-    public float sensitivityY;
-
-    public Transform direction;
-
-    float rotationX;
-    float rotationY;
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
 
     void Start()
     {
-        Cursor.lockState  = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivityX;
-        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivityY;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        rotationY += mouseX;
-        rotationX -= mouseY;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
