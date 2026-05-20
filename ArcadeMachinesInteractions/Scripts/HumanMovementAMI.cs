9d231cc789ad45ccaa35fa3a0535b7e097f3503f
DaniilIzman <dan200631@gmail.com>
2026-05-20 20:36:50 +0200

    HumanMovementAMI - Jump
    Implemented a jump feature by adding a new and separate jump key action in the Input System. To achieve this, a new OnJump() method was created to handle jump input independently from movement. Rigidbody.AddForce() was used to apply upward force for jumping, and a jumpForce variable was added for adjustable jump strength. The script now uses FixedUpdate() for more consistent physics calculations, while movement and jumping are handled through separate Input System actions. In this iteration of the code, a check for whether the player is in the air was not added yet, so the jump button can currently be pressed continuously (to be fixed)

--- ArcadeMachinesInteractions/Scripts/HumanMovementAMI.cs
+++ ArcadeMachinesInteractions/Scripts/HumanMovementAMI.cs
@@ -4,17 +4,36 @@ using UnityEngine.InputSystem;
 public class HumanMovementAMI : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 5f;
 
     Vector3 movement;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;  
+    }
 
     public void OnMovement(InputValue value)
     {
         movement = value.Get<Vector3>();
     }
 
-    void Update()
+    public void OnJump(InputValue value)
+    {
+        if (value.Get<float>() > 0.5f)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Debug.Log("Jump triggered!");
+        }
+    }
+
+    void FixedUpdate()
     {
         Vector3 move = transform.right * movement.x + transform.forward * movement.z;
-        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+        Vector3 velocity = move * moveSpeed;
+        
+        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
     }
 }
