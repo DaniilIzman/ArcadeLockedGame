7bb597f64c0c0f718173693ed26f71f3af6cc71f
DaniilIzman <dan200631@gmail.com>
2026-05-21 20:29:43 +0200

    CollisionDetectorGG - Code refactor
    Refactored the code for better readability. Added spacing between lines of code, added headers for the Inspector tab, and grouped variables together

--- GalaxyGlide/Scripts/CollisionDetectorGG.cs.cs
+++ GalaxyGlide/Scripts/CollisionDetectorGG.cs.cs
@@ -1,98 +1,132 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+
 public class CollisionDetectorGG : MonoBehaviour
 {
-    [SerializeField] float LevelReloadDelay = 0f;
-    [SerializeField] float LoadNextLevelDelay = 0f;
-    [SerializeField] AudioClip VictoryAudio;
-    [SerializeField] AudioClip CrashAudio;
-    [SerializeField] ParticleSystem CrashParticleSystem;
-    [SerializeField] ParticleSystem VictoryParticleSystem;
-    AudioSource AudioSource;
+    [Header("Scene Delays")]
+    [SerializeField] float levelReloadDelay = 0f;
+    [SerializeField] float loadNextLevelDelay = 0f;
+
+    [Header("Audio")]
+    [SerializeField] AudioClip victoryAudio;
+    [SerializeField] AudioClip crashAudio;
+
+    [Header("Particles")]
+    [SerializeField] ParticleSystem crashParticleSystem;
+    [SerializeField] ParticleSystem victoryParticleSystem;
+
+    [Header("Debug")]
+    [SerializeField] bool isCollidable = true;
+
+    AudioSource audioSource;
+    MovementGG movement;
+
     bool isControllable = true;
-    bool isCollidable = true;
 
     void Start()
     {
-        AudioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+        movement = GetComponent<MovementGG>();
     }
 
     void Update()
     {
         Debugging();
     }
+
     void OnCollisionEnter(Collision other)
     {
-        if(isControllable == false || !isCollidable)
+        if (!isControllable || !isCollidable)
         {
             return;
         }
-        else
-        {
-            isControllable = true;
-            switch (other.gameObject.tag)
-            {
-                case "Spawn":
-                    Debug.Log("Spawnpoint");
-                    break;
-                case "Finish":
-                    EffectsAfterFinish();
-                    break;
-                default:
-                    EffectsAfterCrash();
-                    break;
-            } 
-        }
 
+        switch (other.gameObject.tag)
+        {
+            case "Spawn":
+
+                Debug.Log("Spawnpoint");
+
+                break;
+
+            case "Finish":
+
+                EffectsAfterFinish();
+
+                break;
+
+            default:
+
+                EffectsAfterCrash();
+
+                break;
+        }
+    }
+
+    void EffectsAfterCrash()
+    {
+        isControllable = false;
+
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashAudio);
+
+        crashParticleSystem.Play();
+
+        movement.enabled = false;
+
+        Invoke(nameof(ReloadLevelScene), levelReloadDelay);
+    }
+
+    void EffectsAfterFinish()
+    {
+        isControllable = false;
+
+        audioSource.Stop();
+        audioSource.PlayOneShot(victoryAudio);
+
+        victoryParticleSystem.Play();
+
+        movement.enabled = false;
+
+        Invoke(nameof(LoadNextLevelScene), loadNextLevelDelay);
     }
 
     void ReloadLevelScene()
     {
-        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int currentScene =
+            SceneManager.GetActiveScene().buildIndex;
+
         SceneManager.LoadScene(currentScene);
-    }
-    void EffectsAfterCrash()
-    {
-        isControllable = false;
-        AudioSource.Stop();
-        AudioSource.PlayOneShot(CrashAudio);
-        CrashParticleSystem.Play();
-        GetComponent<MovementGG>().enabled = false;
-        Invoke("ReloadLevelScene", LevelReloadDelay);
     }
 
     void LoadNextLevelScene()
     {
-        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int currentScene =
+            SceneManager.GetActiveScene().buildIndex;
+
         int nextScene = currentScene + 1;
-        if(nextScene == SceneManager.sceneCountInBuildSettings)
+
+        if (nextScene >= SceneManager.sceneCountInBuildSettings)
         {
             nextScene = 0;
         }
+
         SceneManager.LoadScene(nextScene);
-    }
-    void EffectsAfterFinish()
-    {
-        isControllable = false;
-        AudioSource.Stop();
-        AudioSource.PlayOneShot(VictoryAudio);
-        VictoryParticleSystem.Play();
-        GetComponent<MovementGG>().enabled = false;
-        Invoke("LoadNextLevelScene", LoadNextLevelDelay);
     }
 
     void Debugging()
     {
-        if(Keyboard.current.lKey.isPressed)
+        if (Keyboard.current.lKey.wasPressedThisFrame)
         {
             LoadNextLevelScene();
         }
-        else if(Keyboard.current.cKey.wasPressedThisFrame)
+
+        else if (Keyboard.current.cKey.wasPressedThisFrame)
         {
             isCollidable = !isCollidable;
+
+            Debug.Log("Collisions: " + isCollidable);
         }
     }
 }
