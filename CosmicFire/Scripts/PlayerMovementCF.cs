06592b38f7aa896e3001ed65282c865ee3145d76
DaniilIzman <dan200631@gmail.com>
2026-05-21 21:09:26 +0200

    PlayerMovementCF - Code refactor
    Refactored the code for better readability. Added spacing between lines of code, added headers for the Inspector tab, and grouped variables together

--- CosmicFire/Scripts/PlayerMovementCF.cs
+++ CosmicFire/Scripts/PlayerMovementCF.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerMovementCF : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] float positionSpeed = 0f;
     [SerializeField] float xRestrictionRange = 0f;
     [SerializeField] float yRestrictionRange = 0f;
 
+    [Header("Rotation")]
     [SerializeField] float pitchFactor = 0f;
     [SerializeField] float rollFactor = 0f;
     [SerializeField] float rotationSpeed = 0f;
 
-    Vector2 movement;   
-    
+    Vector2 movement;
+
     void Update()
     {
         PlayerPosition();
@@ -26,12 +29,17 @@ public class PlayerMovementCF : MonoBehaviour
     void PlayerPosition()
     {
         float xOffset = movement.x * positionSpeed * Time.deltaTime;
+
         float pureXpos = transform.localPosition.x + xOffset;
+
         float clampXpos = Mathf.Clamp(pureXpos, -xRestrictionRange, xRestrictionRange);
 
         float yOffset = movement.y * positionSpeed * Time.deltaTime;
+
         float pureYpos = transform.localPosition.y + yOffset;
-        float clampYpos = Mathf.Clamp(pureYpos,-yRestrictionRange, yRestrictionRange);
+
+        float clampYpos = Mathf.Clamp(pureYpos, -yRestrictionRange, yRestrictionRange);
+
         transform.localPosition = new Vector3(clampXpos, clampYpos, 0f);
     }
 
@@ -39,7 +47,9 @@ public class PlayerMovementCF : MonoBehaviour
     {
         float roll = -rollFactor * movement.x;
         float pitch = -pitchFactor * movement.y;
+
         Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
-        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation,  rotationSpeed * Time.deltaTime);
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
