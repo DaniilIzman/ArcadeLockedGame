40712bea2d8dba133cb19cfbd631de54ea498451
DaniilIzman <dan200631@gmail.com>
2026-05-21 13:06:00 +0200

    ArcadeMachineAMI - Functionality
    Implemented a basic functionality for changing the game scene upon interacting with the arcade machine. When the player enters the trigger area, they are prompted to press E to start the game, and after a short delay, the scene changes. Appropriate console outputs were also added. In the current iteration of the code, debug lines fire multiple times in the console, and functions are also triggered multiple times while the player remains inside the trigger area (to be fixed)

--- ArcadeMachinesInteractions/Scripts/ArcadeMachIneAMI.cs
+++ ArcadeMachinesInteractions/Scripts/ArcadeMachIneAMI.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ArcadeMachIneAMI : MonoBehaviour
+public class ArcadeMachineAMI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] string gameScene = "GameScene";
+    [SerializeField] float loadDelay = 2f;
+    
+    bool playerNear = false;
+    bool activated = false;
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            playerNear = true;
+            Debug.Log("Press E to play");
+        }
     }
 
-    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = false;
+        }
+    }
+
     void Update()
     {
-        
+        if (playerNear && Input.GetKeyDown(KeyCode.E) && !activated)
+        {
+            activated = true;
+            Debug.Log("Loading game...");
+            Invoke("LoadGame", loadDelay);
+        }
+    }
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene(gameScene);
     }
 }
