56ad19100e3db902f1166e9daab62436341db409
DaniilIzman <dan200631@gmail.com>
2026-05-22 17:11:40 +0200

    SceneTransitionManagerAMI - Functionality
    Created a persistent scene transition manager with delayed scene loading functionality. To achieve this, a new SceneTransitionManagerAMI singleton was added for centralized scene control, and DontDestroyOnLoad was used to keep the manager persistent across scenes. New methods for loading scenes by name and build index were added, along with support for delayed scene loading through coroutines and WaitForSeconds. Additional functionality for reloading the current scene and loading the next scene with automatic wrap back to the first scene was also implemented. Duplicate manager instances are prevented through the singleton pattern, and scene loading logic was fixed to correctly use scene indices instead of string conversion

--- ArcadeMachinesInteractions/Scripts/SceneTransitionManagerAMI.cs
+++ ArcadeMachinesInteractions/Scripts/SceneTransitionManagerAMI.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneTransitionManagerAMI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SceneTransitionManager instance;
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(string sceneName, float delay = 0f)
     {
-        
+        StartCoroutine(LoadSceneCoroutine(sceneName, delay));
+    }
+
+    public void ReloadCurrentScene(float delay = 0f)
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadSceneCoroutine(currentScene.ToString(), delay));
+    }
+
+    public void LoadNextScene(float delay = 0f)
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+
+        if (nextScene >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextScene = 0;
+        }
+
+        StartCoroutine(LoadSceneCoroutine(nextScene.ToString(), delay));
+    }
+
+    IEnumerator LoadSceneCoroutine(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
     }
 }
