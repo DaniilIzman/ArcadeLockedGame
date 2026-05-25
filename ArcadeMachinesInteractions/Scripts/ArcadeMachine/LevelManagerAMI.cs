16e8bf4abe56780289ee498c9f8059b8bf28c4e0
DaniilIzman <dan200631@gmail.com>
2026-05-25 20:35:19 +0200

    Arcade room - Level Manager
    Merged all level manager scripts for the three arcade games into a single script

---
+++ ArcadeMachinesInteractions/Scripts/ArcadeMachine/LevelManagerAMI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LevelManagerAMI : MonoBehaviour
+{
+    [System.Serializable]
+    public class Level
+    {
+        public string levelName;
+        public string sceneName;
+    }
+
+    [SerializeField] Level[] levels;
+
+    public Level[] GetLevels() { return levels; }
+
+    public Level GetLevel(int index)
+    {
+        if (index >= 0 && index < levels.Length)
+            return levels[index];
+
+        return null;
+    }
+}
