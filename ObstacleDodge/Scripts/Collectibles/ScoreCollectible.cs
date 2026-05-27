888d0577cb2b31ae91ab33ba370884840236dfe1
DaniilIzman <dan200631@gmail.com>
2026-05-27 21:57:15 +0200

    ScoreCollectible - Code refactor
    Refactored the code for better readability and simplified the scoring logic. Score is now added directly when the player picks up a score collectible

--- ObstacleDodge/Scripts/Collectibles/ScoreCollectible.cs
+++ ObstacleDodge/Scripts/Collectibles/ScoreCollectible.cs
@@ -1,35 +1,15 @@
 using UnityEngine;
-using System.Collections;
+
 public class ScoreCollectible : MonoBehaviour
 {
-    [SerializeField] float MyScoreMultiplier = 0f;
-    [SerializeField] bool ScoreTimerActivate;
-    [SerializeField] float ScoreMultiplierTimer = 0f;
-    ScoringOD PlayerScore;
-
-    void Awake()
-    {
-        PlayerScore = FindAnyObjectByType<ScoringOD>();
-    }
-    IEnumerator ResetScoreMultiplier(float originalMultiplier, float time)
-    {
-        yield return new WaitForSeconds(time);
-        PlayerScore.ScoreMultiplier = originalMultiplier;
-    }
+    [SerializeField] int scoreAmount = 100;
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            float originalMultiplier = PlayerScore.ScoreMultiplier;
-            PlayerScore.ScoreMultiplier = MyScoreMultiplier;
-            if(ScoreTimerActivate == true && ScoreMultiplierTimer > 0f && MyScoreMultiplier > 0f)
-            {
-                Debug.Log("Score multiplier activated!");
-                PlayerScore.StartCoroutine(ResetScoreMultiplier(originalMultiplier, ScoreMultiplierTimer));
-            }
+            ScoringOD.instance.AddScore(scoreAmount);
             Destroy(gameObject);
-            
         }
     }
 }
