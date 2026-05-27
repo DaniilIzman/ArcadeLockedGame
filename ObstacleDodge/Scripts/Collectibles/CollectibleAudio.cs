ec371d56557203458fe9e31d02aff06dd645b558
DaniilIzman <dan200631@gmail.com>
2026-05-27 23:20:47 +0200

    CollectibleAudio
    Implemented a script for handling collectible pickup audio

---
+++ ObstacleDodge/Scripts/Collectibles/CollectibleAudio.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CollectibleAudio : MonoBehaviour
+{
+    [SerializeField] AudioClip collectSound;
+    [SerializeField] float volume = 0.7f;
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void PlayCollectSound()
+    {
+        if (collectSound != null)
+        {
+            audioSource.PlayOneShot(collectSound, volume);
+        }
+    }
+}
