ea80368859c5de4ccb4e6a2e634ba42351127da1
DaniilIzman <dan200631@gmail.com>
2026-05-28 11:49:04 +0200

    HpCollectible - Pickup audio

--- ObstacleDodge/Scripts/Collectibles/HPCollectible.cs
+++ ObstacleDodge/Scripts/Collectibles/HPCollectible.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
+
 public class HPCollectible : MonoBehaviour
 {
     [SerializeField] int AdditionalBumps = 0;
+
     HealthOD PlayerHealth;
+    CollectibleAudio collectibleAudio;
+
     void Awake()
     {
         PlayerHealth = FindAnyObjectByType<HealthOD>();
     }
+
+    void Start()
+    {
+        collectibleAudio = GetComponent<CollectibleAudio>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            if(PlayerHealth.bumpCounter > 0)
+            if (collectibleAudio != null)
+                collectibleAudio.PlayCollectSound();
+
+            if (PlayerHealth.bumpCounter > 0)
             {
                 PlayerHealth.bumpCounter -= AdditionalBumps;
                 PlayerHealth.bumpCounter = Mathf.Max(PlayerHealth.bumpCounter, 0);
