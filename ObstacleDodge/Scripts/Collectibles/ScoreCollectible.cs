d0b3fb6c98f7687d53de0e3795dcc8a458ea3000
DaniilIzman <dan200631@gmail.com>
2026-05-28 11:51:35 +0200

    ScoreCollectible - Pickup audio

--- ObstacleDodge/Scripts/Collectibles/ScoreCollectible.cs
+++ ObstacleDodge/Scripts/Collectibles/ScoreCollectible.cs
@@ -4,10 +4,20 @@ public class ScoreCollectible : MonoBehaviour
 {
     [SerializeField] int scoreAmount = 100;
 
+    CollectibleAudio collectibleAudio;
+
+    void Start()
+    {
+        collectibleAudio = GetComponent<CollectibleAudio>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (collectibleAudio != null)
+                collectibleAudio.PlayCollectSound();
+
             ScoringOD.instance.AddScore(scoreAmount);
             Destroy(gameObject);
         }
