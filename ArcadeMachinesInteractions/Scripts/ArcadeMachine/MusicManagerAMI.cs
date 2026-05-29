8068fb4ca95aa5783b62ee0fc76915e7cc16fa65
DaniilIzman <dan200631@gmail.com>
2026-05-29 17:10:35 +0200

    MusicManagerAMI - Functionality

--- ArcadeMachinesInteractions/Scripts/ArcadeMachine/MusicManagerAMI.cs
+++ ArcadeMachinesInteractions/Scripts/ArcadeMachine/MusicManagerAMI.cs
@@ -2,15 +2,79 @@ using UnityEngine;
 
 public class MusicManagerAMI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Playlist")]
+    [SerializeField] private AudioClip[] clips;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float volume = 0.5f;
+
+    [Header("Settings")]
+    [SerializeField] private bool shuffleClips = true;
+
+    private AudioSource audioSource;
+    private int currentClipIndex = -1;
+
+    void Awake()
     {
-        
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = false;
+        audioSource.playOnAwake = false;
+        audioSource.volume = volume;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        if (clips != null && clips.Length > 0)
+        {
+            if (shuffleClips)
+            {
+                ShuffleClips(clips);
+            }
+            PlayNextClip();
+        }
+    }
+
     void Update()
     {
-        
+        // Automatically play the next clip when the current one finishes
+        if (clips != null && clips.Length > 0 && !audioSource.isPlaying)
+        {
+            PlayNextClip();
+        }
+    }
+
+    private void PlayNextClip()
+    {
+        if (clips == null || clips.Length == 0) return;
+
+        currentClipIndex = (currentClipIndex + 1) % clips.Length;
+
+        AudioClip clip = clips[currentClipIndex];
+        if (clip == null) return;
+
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+    private void ShuffleClips(AudioClip[] clipsToShuffle)
+    {
+        for (int i = clipsToShuffle.Length - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            AudioClip temp = clipsToShuffle[i];
+            clipsToShuffle[i] = clipsToShuffle[j];
+            clipsToShuffle[j] = temp;
+        }
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+
+    public void SetVolume(float newVolume)
+    {
+        volume = Mathf.Clamp01(newVolume);
+        audioSource.volume = volume;
     }
 }
